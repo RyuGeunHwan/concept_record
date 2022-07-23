cfb5e51ba3f06055cd3193160f9ff695fb7fed77
RyuGeunHwan <h46344@naver.com>
2022-07-24 00:05:32 +0900

    feat : 하샤드 수 업로드

---
+++ 프로그래머스/Lv1/하샤드 수.java
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(int x) {
+    bool answer = true;
+    int sum = 0;
+    int harshad = x;
+    
+    while(harshad>0){
+        sum += harshad % 10;
+        harshad /= 10;
+    }
+    if(x%sum == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
