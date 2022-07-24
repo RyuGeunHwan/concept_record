ad06e5edbe5226d3a6ff4079c60fb2d69072eba2
RyuGeun <h46344@naver.com>
2022-07-25 02:06:31 +0900

    feat : 정수 내림차순으로 배치하기 upload

---
+++ 프로그래머스/Lv1/정수 내림차순으로 배치하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long solution(long n) {
+        char[] sort = String.valueOf(n).toCharArray();
+        //.toCharArray() : 자바 toCharArray() 메소드는 문자열을 char형 배열로 바꿔준다. 반환되는 배열의 길이는 문자열의 길이와 같다.
+        // String.valueOf(n) : long(정수형)타입을 String형으로 변환
+
+// 선택 정렬 시작
+	int tmp = 0;
+	for(int i=0; i<sort.length; i++) {
+		for(int j=i+1; j<sort.length; j++) {
+			if(sort[i] < sort[j]) {
+				tmp = sort[i];
+				sort[i] = sort[j];
+				sort[j] = (char) tmp;
+			}
+		}
+	}
+	
+	return Long.parseLong(String.valueOf(sort));
+    }
+}
